0d353b5208ef7eb4a12ba4f9df0dff354d96f54a
Baxtiyor70 <136648262+Baxtiyor70@users.noreply.github.com>
2023-06-21 19:30:12 +0500

    setup

--- products/models.py
+++ products/models.py
@@ -3,8 +3,9 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=255, null=True)
+    category = models.ForeignKey('products.Category', on_delete=models.CASCADE)
     price = models.CharField(max_length=30, null=True)
-    discount_price = models.CharField(max_length=30,null=True)
+    discount_price = models.CharField(max_length=30, null=True)
     description = models.TextField()
     color = models.CharField(max_length=15, null=True)
     size = models.CharField(max_length=5, null=True)
@@ -14,14 +15,10 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+
 class Category(models.Model):
     name = models.CharField(max_length=255)
-    description = models.TextField(null=True,blank=True)
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.name
-
-
-
-
-

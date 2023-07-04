a8dc77edb3f8a8fcd41c23309c827dea05a541b6
Baxtiyor70 <136648262+Baxtiyor70@users.noreply.github.com>
2023-07-04 14:18:35 +0500

    add order and order_detail table

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,42 @@
-# from django.db import models
-#
-#
-# class OrderDetail(models.Model):
-#     product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
+from django.db import models
+from account.models import CustomUser
+from products.models import Product
+
+
+class Order(models.Model):
+    STATUES = [
+
+        ('PENDING', 'pending'),
+        ('INPROGRESS', 'inprogress'),
+        ('COMPLATED', 'complated'),
+        ('CANCELED', 'canceled')
+    ]
+
+    customor = models.ForeignKey('account.CustomUser', on_delete=models.SET_NULL, null=True, related_name='orders')
+    order_date = models.DateTimeField(auto_now_add=True)
+    required_date = models.DateTimeField(null=True)
+    shipped_date = models.DateTimeField(null=True)
+    canceled_data = models.DateTimeField(null=True)
+    status = models.CharField(max_length=10, choices=STATUES)
+
+    def __str__(self):
+        return f'{self.customor.__str__()} --order_id :{self.id}'
+
+    def item_count(self):
+        return self.details.count()
+
+    def total_price(self):
+        return sum([i.total_price() for i in self.details.all()])
+
+
+class OrderDetail(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='details')
+    product = models.ForeignKey('products.Product', on_delete=models.SET_NULL,null=True, related_name='orders')
+    quantity = models.IntegerField(default=1)
+
+    def total_price(self):
+        return self.product.price*self.quantity
+
+    def __str__(self):
+
+        return f'order:{self.order} -- {self.product} --quantity : {self.quantity}'

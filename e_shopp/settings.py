4e2f368d30fc7290cbf74e5e7ce0f4d6480ccdf0
Baxtiyor70 <136648262+Baxtiyor70@users.noreply.github.com>
2023-07-04 13:47:19 +0500

    some change

--- e_shopp/settings.py
+++ e_shopp/settings.py
@@ -81,19 +81,19 @@ WSGI_APPLICATION = 'e_shopp.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'e_shopp_db',
-        'USER': 'e_shopp_user',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'e_shopp_db',
+#         'USER': 'e_shopp_user',
+#         'PASSWORD': 'password',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+ }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
